a0a3956f2090142b932a420c3d09ef9ae82a01f6
Atul Solanki -X (atsolank) <atsolank@cisco.com>
2025-09-04 17:55:50 +0530

    Initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas, database, auth
+models.Base.metadata.create_all(bind=database.engine)
+app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to DevOps Notes App!"}
+@app.post("/register", response_model=schemas.UserResponse)
+def register(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    hashed_pw = auth.hash_password(user.password)
+    new_user = models.User(username=user.username, email=user.email, hashed_password=hashed_pw)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+@app.post("/login")
+def login(user: schemas.UserCreate, db: Session = Depends(database.get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if not db_user or not auth.verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    token = auth.create_access_token({"sub": db_user.email})
+    return {"access_token": token, "token_type": "bearer"}
