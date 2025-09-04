eb0dcf35919b818f538c81a5b50f9a6576532472
Atul Solanki <111354090+atsolank@users.noreply.github.com>
2025-09-04 19:04:05 +0530

    Delete devops-notes-app directory

--- devops-notes-app/README.md
+++
@@ -1,8 +0,0 @@
-# DevOps Notes App (Day 3)
-
-This is a 3-tier application (React + FastAPI + Postgres).
-
-## Run locally
-
-```bash
-docker-compose up --build
--- devops-notes-app/backend/app/schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class UserCreate(BaseModel):
-    username: str
-    password: str
-
-class NoteCreate(BaseModel):
-    title: str
-    content: str
